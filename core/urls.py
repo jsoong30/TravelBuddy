25ce96f06649bb576c0679798d650c02896f95c8
Jackson J Soong <jacksonjsoong@gmail.com>
2025-04-05 19:37:39 -0400

    Signup, login, and logout

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import home, signup
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('itinerary/', views.itinerary, name='itinerary'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('signup/', signup, name='signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth.views import LogoutView
 
 def home(request):
     context = {'page_title': 'Welcome to Travel Buddy'}
@@ -8,4 +10,13 @@ def home(request):
 def itinerary(request):
     return HttpResponse("Itinerary page coming soon!")
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()  # Saves the new user to the database
+            return redirect('login')  # Redirect after successful signup
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
